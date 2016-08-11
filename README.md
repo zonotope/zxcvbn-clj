ca84e9c9c7b9b647525f34b5ce7087119236cb00
ben lamothe <zonotope@gmail.com>
2016-08-10 20:39:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# zxcvbn-clj
+
+A clojure api for [zxcvbn](https://github.com/dropbox/zxcvbn)
+
+## Usage
+
+`(zxcvbn.core/zxcvbn password)`
+
+## License
+
+Copyright © 2016 ben lamothe
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject zonotope/zxcvbn-clj "0.1.0-SNAPSHOT"
+  :description "zxcvbn for clojure"
+
+  :url "https://github.com/zonotope/zxcvbn-clj"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.nulab-inc/zxcvbn "1.1.4"]])
